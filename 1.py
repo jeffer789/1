35d371da29af96e028d4d96af51a00e2809a64e2
ieffer789 <2419542331@qq.com>
2019-06-19 21:17:37 +0800

    睡吧

---
+++ 1.py
@@ -0,0 +1,8 @@
+import sys
+form PyQt5.Qtwidgets import Qwidget, QApplication
+
+if _name_==""_main_"":
+        app=QApplication(sys.argv)
+        q=QWidget()
+        q.show()
+        sys.exit(app.exec_())
